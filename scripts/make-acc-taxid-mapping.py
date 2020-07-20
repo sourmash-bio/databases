f5bc48519b87020d48c8abaae685b7c87b48aa07
Luiz Irber <luiz.irber@gmail.com>
2020-07-19 17:36:51 -0700

    add LCA rule, calculate lineages, and skip missing data in FTP

---
+++ scripts/make-acc-taxid-mapping.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python
+"""
+Take an NCBI 'assembly_summary.txt' file and print to
+stdout the accessions and their associated taxids.
+"""
+
+import argparse
+import csv
+import gzip
+
+
+def main():
+    p = argparse.ArgumentParser()
+    p.add_argument('summary')
+    args = p.parse_args()
+
+    with gzip.open(args.summary, 'rt') as fp:
+        fp.readline() # skip first line
+        fp.read(2) # skip initial comment in header
+        data = csv.DictReader(fp, delimiter='\t')
+        for row in data:
+            # --split-identifiers in `sourmash lca index` doesn't behave
+            # well with version, so remove it
+            accession = row['assembly_accession'].split('.')[0]
+            taxid = row['taxid']
+            print(f'{accession},{taxid}')
+
+
+if __name__ == '__main__':
+    main()
